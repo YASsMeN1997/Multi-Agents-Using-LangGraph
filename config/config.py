74ec3954c9ed8350956850ea0a2e16b8f40d8f4a
Yassmen Youssef <97041680+YASsMeN1997@users.noreply.github.com>
2025-03-20 00:37:34 +0000

    set env var

---
+++ config/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+class Config:
+    os.environ["GROQ_API_KEY"] = os.getenv("GROK_API_KEY")
+    os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
+    os.environ["SCRAPGRAPH_API_KEY"] = os.getenv("SCRAPGRAPH_API_KEY")
+    os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
+    os.environ["LANGSMITH_TRACING"] = "true"
